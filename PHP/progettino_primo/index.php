ba493295dac279ca6f754a99a9c9694656b50e45
Matteo Beccari <98811589+MatteoBeccari05@users.noreply.github.com>
2024-11-21 15:15:21 +0100

    primo progetto php

---
+++ PHP/progettino_primo/index.php
@@ -0,0 +1,23 @@
+<?php
+//echo 'Ciao Mondo!';   // echo è un costrutto
+$var1 = 'Ciao Mondo';
+
+
+/* istruzioni e dati */
+
+
+/* integer - double - boolean - string */
+$count = 10;
+$list_price = 23.67;
+$first_name = 'Matteo';
+$first_name2 = "Alice";
+$is_valid = true;
+$microwave = 3.9e-12;
+echo PHP_INT_MAX;
+echo '<br>';   //per andare a capo
+echo PHP_INT_MIN;
+echo '<br>';
+
+echo 'CIAO MARCELLONE TARGULLO';
+
+
