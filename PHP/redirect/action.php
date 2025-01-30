388436424476809c436059bba2514412b96a6963
Matteo Beccari <98811589+MatteoBeccari05@users.noreply.github.com>
2025-01-30 12:10:40 +0100

    risolve il problema di doppia scrittura

---
+++ PHP/redirect/action.php
@@ -0,0 +1,8 @@
+<?php
+$lname = $_POST['lname'];
+$fname = $_POST['fname'];
+
+header('Location:confirm.html');  //per non avere problemi di scrittura doppia
+
+
+echo "Dati inseriti correttamente";
